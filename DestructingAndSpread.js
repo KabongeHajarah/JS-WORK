09ccd81fd668c889a68912c9e68a813cf7dc05b5
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-07 00:59:47 +0300

    Spread Operator and Exercises

--- DestructingAndSpread.js
+++ DestructingAndSpread.js
@@ -204,3 +204,184 @@ for (const {task, time, completed} of todoList){
           
           const copiedUser = {...user}
           console.log(copiedUser)
+
+//Modifying or changing the object while copying
+
+  const copiedUser2 = {...user, title:'instructor'}
+  console.log(copiedUser2)
+
+
+      //Spread operator with arrow function
+// Whenever we like to write an arrow function which takes unlimited number of arguments we use a spread operator. 
+// If we use a spread operator as a parameter, 
+// the argument passed when we invoke a function will change to an array.
+const sumAllNums=(...args)=>{
+    console.log(args );
+    let sum=0
+    for (const num of args) {
+        sum+=num
+    }
+    return sum
+}
+
+console.log( sumAllNums(1,2,3,4,5));
+
+
+//Exercises
+// Exercises: Level 1
+const constants = [2.72, 3.14, 9.81, 37, 100]
+const countries1 = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const rectangle1 = {
+  width1: 20,
+ height1: 10,
+ area1: 200, 
+perimeter1: 60
+ }
+ const users = [
+{
+  name:'Brook',
+ scores:75,
+ skills:['HTM', 'CSS', 'JS'],
+  age:16
+},
+{
+  name:'Alex',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'David',
+  scores:75,
+  skills:['HTM', 'CSS'],
+  age:22
+},
+{
+  name:'John',
+  scores:85,
+  skills:['HTML'],
+  age:25
+},
+{
+  name:'Sara',
+  scores:95,
+  skills:['HTM', 'CSS', 'JS'],
+  age: 26
+},
+{
+  name:'Martha',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'Thomas',
+  scores:90,
+  skills:['HTM', 'CSS', 'JS'],
+  age:20
+}
+]
+// Destructure and assign the elements of constants array 
+// to e1, pis, gravity, humanBodyTemp, waterBoilingTemp.
+let [e1,pis,gravitys,humanBodyTemp,waterBoilingTemp]=constants
+console.log(e1,pis,gravitys,humanBodyTemp,waterBoilingTemp);
+// Destructure and assign the elements of countries array to fin, est, sw, den, nor
+const [fin, est, sw, den, nor] = countries1;
+console.log(countries1);
+// Destructure the rectangle object by its properties or keys.
+let{width1,height1,area1,perimeter1}=rectangle1
+console.log(width1,height1,area1,perimeter1);
+
+// Exercises: Level 2
+
+// Iterate through the users array and get all the keys of the object using destructuring
+users.forEach((user) => {
+    const keys = Object.keys(user);
+    console.log(keys);
+  });
+  for (const user of users) {
+    const keys = Object.entries(user);
+    console.log(keys);
+  }
+  
+
+// Find the persons who have less than two skills
+const personsWithLessThanTwoSkills = users.filter((user) => user.skills.length < 2);
+console.log(personsWithLessThanTwoSkills);
+
+
+// Exercises: Level 3
+// Destructure the countries object print name, capital, population and languages of all countries
+// A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+//   const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+//   console.log(name, skills, jsScore, reactScore)
+// David (4) ["HTM", "CSS", "JS", "React"] 90 95
+// Write a function called convertArrayToObject which can convert the array to a structure object.
+//     const students = [
+//         ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+//         ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+//       ]
+
+//     console.log(convertArrayToObject(students))
+//     [
+//       {
+//         name: 'David',
+//         skills: ['HTM','CSS','JS','React'],
+//         scores: [98,85,90,95]
+//       },
+//       {
+//         name: 'John',
+//         skills: ['HTM','CSS','JS','React'],
+//         scores: [85, 80,85,80]
+//       }
+//     ]
+// Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+// Add Bootstrap with level 8 to the front end skill sets
+// Add Express with level 9 to the back end skill sets
+// Add SQL with level 8 to the data base skill sets
+// Add SQL without level to the data science skill sets
+//     const student = {
+//       name: 'David',
+//       age: 25,
+//       skills: {
+//         frontEnd: [
+//           { skill: 'HTML', level: 10 },
+//           { skill: 'CSS', level: 8 },
+//           { skill: 'JS', level: 8 },
+//           { skill: 'React', level: 9 }
+//         ],
+//         backEnd: [
+//           { skill: 'Node',level: 7 },
+//           { skill: 'GraphQL', level: 8 },
+//         ],
+//         dataBase:[
+//           { skill: 'MongoDB', level: 7.5 },
+//         ],
+//         dataScience:['Python', 'R', 'D3.js']
+//       }
+//     }
+// The copied object output should look like this:
+
+//     {
+//     name: 'David',
+//     age: 25,
+//     skills: {
+//       frontEnd: [
+//         {skill: 'HTML',level: 10},
+//         {skill: 'CSS',level: 8},
+//         {skill: 'JS',level: 8},
+//         {skill: 'React',level: 9},
+//         {skill: 'BootStrap',level: 8}
+//       ],
+//       backEnd: [
+//         {skill: 'Node',level: 7},
+//         {skill: 'GraphQL',level: 8},
+//         {skill: 'Express',level: 9}
+//       ],
+//       dataBase: [
+//         { skill: 'MongoDB',level: 7.5},
+//         { skill: 'SQL',level: 8}
+//       ],
+//       dataScience: ['Python','R','D3.js','SQL']
+//     }
+//   }

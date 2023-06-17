57a03c51f1373349edfad050ef0db93649823fed
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-17 07:41:23 +0300

    exercisesFP

--- exercisesFP.js
+++ exercisesFP.js
@@ -1,32 +1,80 @@
-// // const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
-// const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// const products = [
-//   { product: 'banana', price: 3 },
-//   { product: 'mango', price: 6 },
-//   { product: 'potato', price: ' ' },
-//   { product: 'avocado', price: 8 },
-//   { product: 'coffee', price: 10 },
-//   { product: 'tea', price: '' },
-// ]
+ const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
 // Explain the difference between forEach, map, filter, and reduce.
 // Define a callback function before you use it in forEach, map, filter or reduce.
 // Use forEach to console.log each country in the countries array.
+countries.forEach(country=> {
+    console.log(country);
+});
+
 // Use forEach to console.log each name in the names array.
+names.forEach(name=>{
+    console.log(name);
+})
 // Use forEach to console.log each number in the numbers array.
+numbers.forEach(number =>{
+    console.log(number);
+})
 // Use map to create a new array by changing each country to uppercase in the countries array.
+let newArrayCapitalize=countries.map((country)=>country.toUpperCase())
+console.log(newArrayCapitalize);
 // Use map to create an array of countries length from countries array.
+let newArrayLength= countries.map((country)=> country.length)
+console.log(newArrayLength);
 // Use map to create a new array by changing each number to square in the numbers array
+let squareNums= numbers.map(number=> number * number);
+console.log(squareNums);
 // Use map to change to each name to uppercase in the names array
+let namesUppercase= names.map(name=> name.toUpperCase())
+console.log(namesUppercase);
 // Use map to map the products array to its corresponding prices.
+let productsArray= products.map(product=> product.price);
+console.log(productsArray);
 // Use filter to filter out countries containing land.
+let filteredWithLand= countries.filter((country)=> country.includes('land' ));
+console.log(filteredWithLand);
 // Use filter to filter out countries having six character.
+let filteredWithSixCharacters=countries.filter((country)=> country.length ===6);
+console.log(filteredWithSixCharacters);
 // Use filter to filter out countries containing six letters and more in the country array.
+let filteredWithMorethanSix=countries.filter((country)=> country.length >=6);
+console.log(filteredWithMorethanSix);
 // Use filter to filter out country start with 'E';
+let filterOut= countries.filter(country=> !country.startsWith('E'));
+console.log(filterOut);
 // Use filter to filter out only prices with values.
-// Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
+let onlyWithValues= products.filter(product=>! isNaN(product.price) );
+console.log(onlyWithValues);
+// Declare a function called getStringLists which takes an array as a parameter 
+// and then returns an array only with string items.
+ function getStringList(array){
+    let arrayStrings=[];
+    for (let index = 0; index < array.length; index++) {
+        if (typeof(array[index])===String) {
+            arrayStrings.push(array[index]);
+        }
+        
+    }
+return arrayStrings 
+ }
+ let array=[1,2,'hello',true,'sap','Bellah'];
+ console.log(getStringList(array));
 // Use reduce to sum all the numbers in the numbers array.
-// Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
+let sum= numbers.reduce((acc,cur)=>  acc + cur, 0);
+console.log({sum});
+// Use reduce to concatenate all the countries and
+//  to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
+let sentence= countries.reduce((acc,cur)=> acc+=cur )
+console.log(` Estonia,${sentence} are North European Countries` );
 // Explain the difference between some and every
 // Use some to check if some names' length greater than seven in names array
 // Use every to check if all the countries contain the word land
